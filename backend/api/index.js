aa099b37b55a0254bf38051829f26a55655b200c
player127-monster <sachindannagaraja@gmail.com>
2025-08-25 21:21:10 +0530

    6 product.tsx getProducts fetch api url changed

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: "https://all-in-one-rosy.vercel.app" || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
