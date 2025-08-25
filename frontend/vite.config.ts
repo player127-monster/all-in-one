0d2a2d73811cda1c781d09ff4eb69461da0a2e10
player127-monster <sachindannagaraja@gmail.com>
2025-08-25 11:29:38 +0530

    3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://backend4-phi.vercel.app/',
         changeOrigin: true,
       },
     },
